98a6e13f373d2b8cec35b104fe9c96b3fed30e4d
hans <hans.tapia@epitech.eu>
2022-02-10 16:08:29 +0100

    login

--- php/login.php
+++ php/login.php
@@ -1,5 +1,26 @@
 <?php
+ try {
+    $pdo = new PDO('mysql:host=localhost;dbname=twitter_academie;charset=utf8', 'root');
+} catch (Exception $e) {
+    echo "Can't connect to database :" . $e->getMessage();
+}
 
-if (isset($_POST['email']) && isset($_POST['password'])) {
-    
+if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $email = hash("sha512", $_POST["email"].'vive le projet tweet_academy');
+    $pass = hash("sha512", $_POST["password"].'vive le projet tweet_academy');
+
+    $command = "SELECT id FROM users WHERE `password` = '$pass' AND email = '$email'";
+    try {
+        $sth = $pdo->query($command);
+        $res = $sth->fetchAll();
+
+        if ($res["id"]->rowCount() == 1){
+            // TODO
+        } else {
+            // TODO
+        }
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
 }
